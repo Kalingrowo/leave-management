a4b4426eb14026bba9259e25863a601c483d7db1
Rizqon <m.rum.rowi.r@gmail.com>
2020-11-27 09:19:58 +0700

    add user seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456')
+        ]);
+    }
+}
