320a13339fac4f2b05eb221349d4b392e8f00464
Rizqon <m.rum.rowi.r@gmail.com>
2020-11-30 10:37:47 +0700

    add user-access: get-roles, get-permissions, assign-permissions-to-user

---
+++ app/Http/Controllers/UserAccess/UserAccessController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\UserAccess;
+
+use App\Http\Controllers\Controller;
+use App\Models\Permission;
+use App\Models\Role;
+use App\Models\User;
+use Crypt;
+use DB;
+use Illuminate\Http\Request;
+
+class UserAccessController extends Controller
+{
+    /**
+     * @return json
+     */
+    public function getAllPermissions()
+    {
+        $getPersmissions = Permission::get();
+
+        return response()->json([
+            'data' => $getPersmissions
+        ], 200);
+    }
+
+    /**
+     * @return json
+     */
+    public function getAllRoles()
+    {
+        $getPersmissions = Role::get();
+
+        return response()->json([
+            'data' => $getPersmissions
+        ], 200);
+    }
+
+    /**
+     * @param int role_id
+     * @return json
+     */
+    public function getRoleDetail($roleId)
+    {
+        $roleDetail = Role::where('id', $roleId)
+            ->with('permissions')
+            ->first();
+
+        return response()->json([
+            'data' => $roleDetail
+        ], 200);
+    }
+
+    /**
+     * @param int encrypted_user_id
+     * @return json
+     */
+    public function assignPermissionsToUser(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+            $listPermissions = $request->permissions;
+            $targetUser = Crypt::decrypt($request->user_id);
+            $targetUser = User::where('id', $targetUser)->first();
+
+            if (is_null($targetUser)) {
+                throw new Exception("Data tidak ditemukan !", 404);
+            }
+
+            $targetUser->givePermissionsTo($listPermissions);
+            $targetUser->refresh();
+
+            DB::commit();
+            return response()->json([
+                'target_user' => $targetUser
+            ], 200);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json([
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -34,5 +34,6 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('get-all-permissions', [UserAccessController::class, 'getAllPermissions'])->name('get_all_permissions');
         Route::get('get-all-roles', [UserAccessController::class, 'getAllRoles'])->name('get_all_roles');
         Route::get('get-role-detail/{id}', [UserAccessController::class, 'getRoleDetail'])->name('get_role_detail');
+        Route::post('assign-permissions-to-user', [UserAccessController::class, 'assignPermissionsToUser'])->name('assign_permissions_to_user');
     });
 });
