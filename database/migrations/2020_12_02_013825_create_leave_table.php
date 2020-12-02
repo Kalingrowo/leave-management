0a74bb7b7db17080ce7521055c0d0b75dbee53b8
Rizqon <m.rum.rowi.r@gmail.com>
2020-12-02 08:49:41 +0700

    add table leave migration

---
+++ database/migrations/2020_12_02_013825_create_leave_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaveTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('allocated_days')->nullable();
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave');
+    }
+}
